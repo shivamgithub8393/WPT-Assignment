e87b6470e0c56b7311d52c9dcd1b8334db14c3b9
Shivam <shivam.sh9557@gmail.com>
2021-08-05 03:10:39 +0530

    Customer operation Updated

--- public/scripts/customer.js
+++ public/scripts/customer.js
@@ -2,18 +2,25 @@ $(document).ready(() => {
   let url = "/api/customers";
 
   $("#create").click(() => {
+    var id = $("#userid").val();
+    var fname = $("#fname").val();
+    var lname = $("#lname").val();
+    var email = $("#email").val();
+    var contact = $("#contact").val();
+
     var newCustomer = {
-      id: 55,
-      firstname: "Sonu",
-      lastname: "Singh",
-      email: "Sonu.singh@gamil.com",
-      contact: "4567546785",
+      customerid: id,
+      firstname: fname,
+      lastname: lname,
+      email: email,
+      contact: contact,
     };
     $.ajax({
       url: url,
       type: "POST",
       data: newCustomer,
       success: (result) => {
+        console.log(result);
         console.log("Customer Added Successfully");
       },
       error: (err) => {
@@ -30,6 +37,7 @@ $(document).ready(() => {
       success: (result) => {
         console.log(result);
         // Craete table for customer list
+        $("#show").html("");
         $("#show").append(
           `<tr><th>Customer ID</th><th>Name</th><th>Email</th><th>Contact Number</th></tr>`
         );
@@ -40,6 +48,56 @@ $(document).ready(() => {
         });
       },
       error: (err) => {
+        console.log(err);
+      },
+    });
+  });
+
+  // update customer
+  $("#update").click(function () {
+    var id = $("#userid1").val();
+    var fname = $("#fname1").val();
+    var lname = $("#lname1").val();
+    var email = $("#email1").val();
+    var contact = $("#contact1").val();
+
+    var newCustomer = {
+      customerid: id,
+      firstname: fname,
+      lastname: lname,
+      email: email,
+      contact: contact,
+    };
+
+    console.log(newCustomer);
+    //U operation : Update Operation
+    $.ajax({
+      url: url + "/"+newCustomer.customerid,
+      type: "PUT",
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(newCustomer),
+      success: function (result) {
+        console.log(result);
+        console.log("Customer Updated");
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  });
+
+  $("#delete").click(function () {
+    // used to delete customer from server
+    //D operation: Delete operation
+    var id = $("#customerid").val();
+    $.ajax({
+      url: url + "/" + id,
+      type: "DELETE",
+      success: function (result) {
+        console.log("the customer is deleted from database");
+      },
+      error: function (err) {
         console.log(err);
       },
     });
